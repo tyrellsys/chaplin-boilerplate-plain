dde312082ca96bf7024605b73cbba02f866aa02b
mtak3 <mtak@tyrellsys.com>
2014-01-28 12:14:41 +0900

    どこから読み込むかによって相対パスが変わるため、絶対パスに変更しました。
    brunch new gh:tyrellsys/chaplin-boilerplate-plain (DocumentRoot)/assets
    の場合の設定。
    (DocumentRoot)/assetsが変わったら、baseUrlを変更して下さい。

--- js/require.config.js
+++ js/require.config.js
@@ -1,18 +1,18 @@
 // Configure the AMD module loader
 requirejs.config({
     // The path where your JavaScripts are located
-    baseUrl: './js/',
+    baseUrl: '/assets/',
     // Specify the paths of vendor libraries
     paths: {
-        jquery: '../bower_components/jquery/jquery',
-        jqueryui: '../bower_components/jquery-ui/ui/jquery-ui',
-        underscore: '../bower_components/lodash/dist/lodash',
-        backbone: '../bower_components/backbone/backbone',
-        chaplin: '../bower_components/chaplin/chaplin',
-        moment: '../bower_components/momentjs/moment',
-        momentja: '../bower_components/momentjs/lang/ja',
-        handlebars: '../bower_components/handlebars/handlebars',
-        text: '../bower_components/requirejs-text/text'
+        jquery: 'bower_components/jquery/jquery',
+        jqueryui: 'bower_components/jquery-ui/ui/jquery-ui',
+        underscore: 'bower_components/lodash/dist/lodash',
+        backbone: 'bower_components/backbone/backbone',
+        chaplin: 'bower_components/chaplin/chaplin',
+        moment: 'bower_components/momentjs/moment',
+        momentja: 'bower_components/momentjs/lang/ja',
+        handlebars: 'bower_components/handlebars/handlebars',
+        text: 'bower_components/requirejs-text/text'
     },
     // Underscore and Backbone are not AMD-capable per default,
     // so we need to use the AMD wrapping of RequireJS
